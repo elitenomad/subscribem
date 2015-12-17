8c3b262743386ef3cc421318cc613209b35e69ae
elitenomad <stalin.pranava@gmail.com>
2015-12-17 18:35:35 +1100

    Add authentication through warden

--- app/controllers/subscribem/accounts_controller.rb
+++ app/controllers/subscribem/accounts_controller.rb
@@ -4,12 +4,15 @@ module Subscribem
   class AccountsController < ApplicationController
     def new
       @account = Subscribem::Account.new
+      @account.build_owner
     end
 
     def create
       @account = Subscribem::Account.new(account_params)
 
       if @account.save
+        env["warden"].set_user(@account.owner, :scope => :user)
+        env["warden"].set_user(@account, :scope => :account)
         redirect_to subscribem.root_url, notice: "Your account has been successfully created."
       else
         render :new, notice: 'Sorry, account is not saved!!!'
@@ -20,7 +23,11 @@ module Subscribem
     private
 
       def account_params
-        params.require(:account).permit(:name)
+        params.require(:account).permit(:name,
+                                        {:owner_attributes => [
+                                            :email,
+                                            :password,
+                                            :password_confirmation ]})
       end
   end
 end
--- spec/features/accounts/sign_up_spec.rb
+++ spec/features/accounts/sign_up_spec.rb
@@ -5,8 +5,12 @@ feature "Accounts" do
     visit subscribem.root_path
     click_link "Account Sign up"
     fill_in "Name", :with => "Test"
+    fill_in "Email", :with => "subscribem@example.com"
+    fill_in "Password", :with => "welcome"
+    fill_in "Password confirmation", :with => "welcome"
     click_button "Create Account"
     success_message = "Your account has been successfully created."
     expect(page).to have_content(success_message)
+    expect(page).to have_content("Signed in as subscribem@example.com")
   end
 end
